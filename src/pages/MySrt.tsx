e8eb3e0cabbac97963c384a75e282e7fd0367ef6
Lee_gaeun <lgu0521@gmail.com>
2024-10-12 23:33:09 +0900

    rem -> px

--- src/pages/MySrt.tsx
+++ src/pages/MySrt.tsx
@@ -25,25 +25,25 @@ const MySrt = () => {
                     </TableHeader>
                     <TableBody>
                         <Frame1Wrapper>
-                            <Typography16 color="#666666" fontWeight={500} style={{ marginRight: "0.2rem" }}>잔여횟수</Typography16>
-                            <Badge style={{ marginRight: "0.7rem" }}>8회</Badge>
+                            <Typography16 color="#666666" fontWeight={500} style={{ marginRight: "2px" }}>잔여횟수</Typography16>
+                            <Badge style={{ marginRight: "7px" }}>8회</Badge>
                             <Typography12 color="#333333">승차 전 횟수차감 해주세요.</Typography12>
                         </Frame1Wrapper>
-                        <Line style={{ marginTop: "0.5rem" }} />
-                        <Frame2Wrapper style={{ padding: "2rem 0" }}>
-                            <Typography28 style={{ marginRight: "1rem" }}>광주송정</Typography28>
-                            <ChangeImgWrapper src={ChangeImg} alt='round_arrow_right' style={{ marginRight: "3.6rem" }} />
+                        <Line style={{ marginTop: "5px" }} />
+                        <Frame2Wrapper style={{ padding: "20px 0" }}>
+                            <Typography28 style={{ marginRight: "10px" }}>광주송정</Typography28>
+                            <ChangeImgWrapper src={ChangeImg} alt='round_arrow_right' style={{ marginRight: "36px" }} />
                             <Typography28>동탄</Typography28>
                         </Frame2Wrapper>
                         <Line />
-                        <Frame1Wrapper gap={0.3} style={{ padding: "0.9rem 0" }}>
+                        <Frame1Wrapper gap={3} style={{ padding: "0px 0" }}>
                             <Typography14 color="#8F3575" fontWeight={600} ls={-1}>SRT 606</Typography14>
                             <Typography14 color="#292929">광주송정(07:54)</Typography14>
                             <RoundArrowRightIconImg src={RoundArrowRightImg} alt='round_arrow_right' />
                             <Typography14 color="#292929">동탄(09:37)</Typography14>
                         </Frame1Wrapper>
                         <Line />
-                        <Frame1Wrapper gap={0.3} style={{ padding: "0.9rem 0" }}>
+                        <Frame1Wrapper gap={3} style={{ padding: "0px 0" }}>
                             <Typography14 color="#8F3575" fontWeight={600} ls={-1}>SRT 661</Typography14>
                             <Typography14 color="#292929">동탄(16:29)</Typography14>
                           
@@ -53,18 +53,18 @@ const MySrt = () => {
                         </Frame1Wrapper>
                         <Line />
                         <Frame3Wrapper>
-                            <ColumnBox gap={0.5} ai="center" jc="center">
+                            <ColumnBox gap={5} ai="center" jc="center">
                             <div style={{
                                 padding: "6px 3px 6px 0px",
                             }}>
                                 <UsedSrtWrapper src={UsedSrt} alt='not_used_srt' />
                                 </div>
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} color="#333333">20241010</Typography12>
                                     <Typography12 fontWeight={500} color="#333333">07:35:46</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.5} ai="center" jc="center">
+                            <ColumnBox gap={5} ai="center" jc="center">
                             <div style={{
                                 background: "#4C3049",
                                 padding: "6px 3px 6px 0px",
@@ -72,65 +72,65 @@ const MySrt = () => {
                             }}>
                                 <UseingSrtWrapper src={RoundArrowRightUseingImg} alt='useing_srt' />
                                 </div>
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} color="#5F1D4A">20241013</Typography12>
                                     <Typography12 fontWeight={500} color="#5F1D4A">07:40:05</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
-                            <ColumnBox gap={0.9} ai="center" jc="center">
+                            <ColumnBox gap={9} ai="center" jc="center">
                                 <NotUsedSrtImgWrapper src={NotUsedSrtImg} alt='not_used_srt' />
-                                <ColumnBox gap={0.1} ai="center">
+                                <ColumnBox gap={1} ai="center">
                                     <Typography12 fontWeight={500} ls={-0.1} color="#5F1D4A">사용가능</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
                         </Frame3Wrapper>
                         <Line/>
                         <Frame4Wrapper>
-                            <RowBox gap={0.5} ai="center" jc="space-between">
+                            <RowBox gap={5} ai="center" jc="space-between">
                                 <QrCodeImgWrapper src={QrCodeImg} alt='qr_code' />
-                                <ColumnBox gap={0.6}>
+                                <ColumnBox gap={6}>
                                     <RowBox>
                                         <Typography16 color="#333333" fontWeight={500} ls={-1}>이가은/여/</Typography16>
                                         <Typography16 color="#333333" fontWeight={500} ls={-0.7}>(2001/05/21)</Typography16>
@@ -145,14 +145,14 @@ const MySrt = () => {
                             </RowBox>
                             <InfiniteLogoCarousel />
                         </Frame4Wrapper>
-                        <ColumnBox style={{ padding: "0 1.5rem" }}>
-                            <RowBox jc="space-between" style={{ padding: "2.5rem 0 0.8rem 0" }}>
+                        <ColumnBox style={{ padding: "0 15px" }}>
+                            <RowBox jc="space-between" style={{ padding: "25px 0 8px 0" }}>
                                 <Typography18 color="#333333" fontWeight={700} ls={-1}>영수액</Typography18>
                                 <Typography18 color="#8F3575" fontWeight={700} ls={-1}>216,000원</Typography18>
                             </RowBox>
                             <Line />
-                            <Typography16 color="#292929" ls={-1} fontWeight={500} style={{ marginLeft: "0.2rem", marginTop: "2rem" }}>이용안내</Typography16>
-                            <Typography16 color="#292929" ls={-1} style={{ marginTop: "1.5rem" }}>•  구매 시 지정한 열차와 시간표에 기재된 열</Typography16>
+                            <Typography16 color="#292929" ls={-1} fontWeight={500} style={{ marginLeft: "2px", marginTop: "20px" }}>이용안내</Typography16>
+                            <Typography16 color="#292929" ls={-1} style={{ marginTop: "15px" }}>•  구매 시 지정한 열차와 시간표에 기재된 열</Typography16>
                         </ColumnBox>
                     </TableBody>
                 </Table>
@@ -167,27 +167,27 @@ const MySrt = () => {
 }
 
 const GrayButton = styled.div`
-    font-size: 1.8rem;
+    font-size: 18px;
     font-family: 'Roboto';
     font-weight: 500;
     color: #ffffff;
     background: #626262;
-    padding: 1.5rem;
+    padding: 15px;
     text-align: center;
 `
 const PurpleButton = styled.div`
-    font-size: 1.8rem;
+    font-size: 18px;
     font-family: 'Roboto';
     font-weight: 500;
     color: #ffffff;
     background: #8F3575;
-    padding: 1.5rem;
+    padding: 15px;
     text-align: center;
 `
 const PageWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 0.5rem;
+    gap: 5px;
     width: 100%;
     height: 100%;
     background: #f5f5f5;
@@ -198,7 +198,7 @@ const PageHeader = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 1.5rem 2rem;
+    padding: 15px 20px;
     width : 100%;   
     background-color: #ffffff;
 `
@@ -221,23 +221,23 @@ const PageFooter = styled.div`
     
 `
 const Table = styled.div`
-    border-radius: 1.7rem;
+    border-radius: 17px;
     overflow: hidden;
-    padding: 1.5rem 1.5rem;
+    padding: 15px 15px;
 `
 
 const TableHeader = styled.div`
     text-align: center;
-    padding: 1.7rem;
+    padding: 17px;
     background-color: #8F3575;
-    border-radius: 1.7rem 1.7rem 0 0;
+    border-radius: 17px 17px 0 0;
 `
 const TableBody = styled.div`
     background: #EDEDED;
-    padding: 1rem 1.6rem;
+    padding: 10px 16px;
 `
 const Typography = styled.p<{ fontWeight?: number, ls?: number, color?: string }>`
-    font-size: 2rem;
+    font-size: 20px;
     font-family: 'Roboto';
     font-weight: ${props => props.fontWeight ? props.fontWeight : 400};
     color : ${props => props.color ? props.color : '#282828'};
@@ -245,27 +245,27 @@ const Typography = styled.p<{ fontWeight?: number, ls?: number, color?: string }
 `
 
 const Typography20 = styled(Typography)`
-    font-size: 2rem;
+    font-size: 20px;
     font-family: 'Roboto';
     font-weight: 700;
     color: #282828;
     letter-spacing: -5%;
 `
 const Typography18 = styled(Typography)`
-    font-size: 1.8rem;
+    font-size: 18px;
 `
 const Typography16 = styled(Typography)`
-    font-size: 1.6rem;
+    font-size: 16px;
 `
 const Typography14 = styled(Typography)`
-    font-size: 1.4rem;
+    font-size: 14px;
 `
 const Typography12 = styled(Typography) <{ color?: string }>`
-    font-size: 1.2rem;
+    font-size: 12px;
     letter-spacing: -2%;
 `
 const Typography28 = styled(Typography)`
-    font-size: 2.8rem;
+    font-size: 28px;
     font-family: 'Roboto';
     font-weight: 500;
     color: #8F3575;
@@ -273,44 +273,44 @@ const Typography28 = styled(Typography)`
 `
 
 const ArrowLeftIconImg = styled.img`
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 15px;
+    height: 15px;
 `
 
 const RoundArrowRightIconImg = styled.img`
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 15px;
+    height: 15px;
 `
 const ChangeImgWrapper = styled.img`
-    width: 3rem;
-    height: 3rem;
+    width: 30px;
+    height: 30px;
 `
 const NotUsedSrtImgWrapper = styled.img`
-    width: 3.7rem;
+    width: 37px;
     height: auto;    
 `
 
 const UseingSrtWrapper = styled.img`
-    width: 3.7rem;
+    width: 37px;
     height: auto;  
 `
 const UsedSrtWrapper = styled.img`
-    width: 3.7rem;
+    width: 37px;
     height: auto;  
 `
 const QrCodeImgWrapper = styled.img`
-    width: 7rem;
-    height: 7rem;
-    margin-left: 3rem;
+    width: 70px;
+    height: 70px;
+    margin-left: 30px;
 `
 const Badge = styled.div`
-    padding: 0.5rem 1rem;
-    border-radius: 14.5rem;
+    padding: 5px 10px;
+    border-radius: 145px;
     width: fit-content;
     border: 1px solid #CDCDCD;
     background: #ffffff;
     text-align: center;
-    font-size: 1.6rem;
+    font-size: 16px;
     font-family: 'Roboto';
     font-weight: 700;
     color: #82263D;
@@ -320,7 +320,7 @@ const Badge = styled.div`
 const Frame1Wrapper = styled.div<{ gap?: number }>`
     display: flex;
     align-items: center;
-    gap: ${props => props.gap ? props.gap : '0'}rem;
+    gap: ${props => props.gap ? props.gap : '0'}px;
 `
 
 const Frame2Wrapper = styled.div`
@@ -331,20 +331,20 @@ const Frame2Wrapper = styled.div`
 const Frame3Wrapper = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-    padding: 0.5rem 0.9rem 0.6rem 0.9rem;
-    row-gap: 1.5rem;
+    padding: 5px 0px 6px 0px;
+    row-gap: 15px;
 `
 const Frame4Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     gap: 25px;
     background: #ffffff;
-    padding: 1.1rem 0 0rem 0;
+    padding: 11px 0 0 0;
     border-top: 1px solid #CCCCCC;
 `
 const Line = styled.div`
     width: 100%;
-    height: 0.1rem;
+    height: 1px;
     background: #D9D9D9;
 `
 
@@ -353,12 +353,12 @@ const ColumnBox = styled.div<{ gap?: number, ai?: 'center' | 'flex-start' | 'fle
     flex-direction: column;
     align-items: ${props => props.ai ? props.ai : 'initial'};
     justify-content: ${props => props.jc ? props.jc : 'initial'};
-    gap: ${props => props.gap ? props.gap : 0}rem;
+    gap: ${props => props.gap ? props.gap : 0}px;
 `
 const RowBox = styled.div<{ gap?: number, ai?: 'center' | 'flex-start' | 'flex-end' | 'stretch' | 'baseline' | 'initial' | 'inherit', jc?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly' | 'initial' | 'inherit' }>`
     display: flex;
     align-items: ${props => props.ai ? props.ai : 'initial'};
     justify-content: ${props => props.jc ? props.jc : 'initial'};
-    gap: ${props => props.gap ? props.gap : 0}rem;
+    gap: ${props => props.gap ? props.gap : 0}px;
 `
 export default MySrt;

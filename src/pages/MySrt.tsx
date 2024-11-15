82634c2cc10ffc7919a557120dfd540aa92bca43
Lee_gaeun <lgu0521@gmail.com>
2024-11-16 06:34:38 +0900

    v-

--- src/pages/MySrt.tsx
+++ src/pages/MySrt.tsx
@@ -86,7 +86,7 @@ const MySrt = () => {
                                     <UseingSrtWrapper src={RoundArrowRightUseingImg} alt='useing_srt' />
                                 </div>
                                 <ColumnBox gap={0.1} ai="center">
-                                    <Typography12 fontWeight={500} color="#5F1D4A">202411016</Typography12>
+                                    <Typography12 fontWeight={500} color="#5F1D4A">20241116</Typography12>
                                     <Typography12 fontWeight={500} color="#5F1D4A">06:29:05</Typography12>
                                 </ColumnBox>
                             </ColumnBox>
